69b017b51cbde7f7b24d265577914489b8ae69a3
gargavi-oss <gargavi2006@outlook.com>
2026-03-05 14:21:42 +0530

    backend changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,9 +6,11 @@ const pool = mysql.createPool({
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '',
     database: process.env.DB_NAME || 'ecommerce_store',
+    port: process.env.DB_PORT || 22749,   // ✅ Add this line
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
+   
 });
 
 // Test connection
